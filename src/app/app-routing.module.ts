8d9571d5e7c50d5eb865ab110bca35b2acb9f2d4
devlp121 <okirialbert@gmail.com>
2019-02-05 21:36:01 +0300

    components creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './subs/portfolio/portfolio.component';
+import { WorksComponent } from './subs/works/works.component';
+import { GalleryComponent } from './subs/gallery/gallery.component';
+import { GarageComponent } from './subs/garage/garage.component';
+import { BlogsComponent } from './subs/blog/blogs/blogs.component';
+import { PostComponent } from './subs/blog/post/post.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {
+    path: '', component: HomeComponent, children: [
+      {
+        path: '', children: [
+          { path: 'portfolio', component: PortfolioComponent },
+          { path: 'works', component: WorksComponent },
+          { path: 'gallery', component: GalleryComponent },
+          { path: 'garage', component: GarageComponent },
+          {
+            path: 'blog', component: BlogsComponent, children: [
+              {
+                path: '', children: [
+                  { path: 'posts', component: PostComponent }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { PortfolioComponent } from './subs/portfolio/portfolio.component';
+import { WorksComponent } from './subs/works/works.component';
+import { GalleryComponent } from './subs/gallery/gallery.component';
+import { PostComponent } from './subs/blog/post/post.component';
+import { BlogsComponent } from './subs/blog/blogs/blogs.component';
+import { GarageComponent } from './subs/garage/garage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PortfolioComponent,
+    WorksComponent,
+    GalleryComponent,
+    PostComponent,
+    BlogsComponent,
+    GarageComponent
   ],
   imports: [
     BrowserModule,
